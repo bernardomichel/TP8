1c131599768cb2aa3aa7c25c165f2942dcb11c43
bernardomichel <bmichel@itba.edu.ar>
2015-05-19 14:38:28 -0300

    Correción de errores del header

--- funciones.h
+++ funciones.h
@@ -1,2 +1,11 @@
-int onlyletter(*char p2char);
+#ifndef FUNCIONES_H
+
+#define FUNCIONES_H
+//Función sort, recibe dos arreglos de char y la longitud de cada uno.  //
+// Devuelve si se puede formar el sugundo string con el primer		//
+int onlyletter(char* p2char);		
+
+//funcion onlyletter, recibe un puntero al principio de un string//
+//devuleve un cero si el string solo contiene letras, caso contrario devuelve -1//
 int sort(char arreglo1[], char arreglo2[],int num1,int num2);
+#endif
